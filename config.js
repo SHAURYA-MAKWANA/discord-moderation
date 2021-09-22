290d54f44e4d53061882dea83a0ee71747290761
ali-harini101 <82109937+ali-harini101@users.noreply.github.com>
2021-09-22 18:08:43 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.Prefix = `$`;
+exports.Token = `ODkwMTY3NDg4OTEyOTQ5MjU4.YUr3Ew.QQYrORV4oObIRW_mk2Rk3erB9Pk`;
+exports.Color = `RANDOM`;
---
+++ main.py
@@ -0,0 +1,4 @@
+from keep_alive import keep_alive
+keep_alive()
+token = os.environ.get("DISCORD_BOT_SECRET")
+client.run(ODkwMTY3NDg4OTEyOTQ5MjU4.YUr3Ew.QQYrORV4oObIRW_mk2Rk3erB9Pk)
